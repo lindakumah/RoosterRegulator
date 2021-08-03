da3e329fd57949e2253903739cfb084c2f12c59b
Enoch Gyasi <lindakumah03@gmail.com>
2021-08-03 15:25:00 +0000

    Rooster Regulator Project

---
+++ index_test.js
@@ -0,0 +1,50 @@
+const assert = require('assert');
+const Rooster = require('../index');
+
+describe('Rooster', () => {
+  describe('.announceDawn', () => {
+    it('returns a rooster call', () => {
+      //Setup
+      const expected = 'cock-a-doodle-doo!';
+
+      //Exercise
+      const actual = Rooster.announceDawn();
+
+      //Verify
+      assert.equal(actual, expected);
+    });
+  });
+  describe('.timeAtDawn', () => {
+    it('returns its argument as a string', () => {
+      //Setup
+      const inputNumber = 14;
+      const expected = '14';
+
+      //Exercise
+      const actual = Rooster.timeAtDawn(inputNumber);
+
+      //Verify
+      assert.ok(actual,expected);
+    });
+    it('throws a range error if passed a number less than 0', () => {
+      //Setup
+      const inputNumber = -1;
+      const expected = RangeError;
+
+      //Verify
+      assert.throws(() => {
+        Rooster.timeAtDawn(inputNumber);
+      }, expected);
+    });
+    it('throws a range error if passed a number more than 23', () => {
+      //Setup
+      const inputNumber = 25;
+      const expected = RangeError;
+
+      //Verify
+      assert.throws(() => {
+        Rooster.timeAtDawn(inputNumber);
+      }, expected);
+    });
+  });
+});
